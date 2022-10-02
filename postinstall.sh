872dea6b68bb3e67eae768c5af230bfa9b9246f4
Diego Lima <lima.ds@outlook.com>
2022-10-02 17:57:09 -0300

    Updated install script

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,7 @@ PKG_CONFIG_PATH=/app/lib:/app/lib/pkgconfig
 ICON_SRC=/run/build/Queries/data/icons/hicolor
 ICON_DST=/app/share/icons/hicolor
 FLATPAK_ICON_DST=/app/share/app-info/icons/flatpak
-CARGO_TARGET_PATH=/run/build/Queries/target/debug
+CARGO_TARGET_PATH=/run/build/Queries/target/release
 
 mkdir -p ${ICON_DST}/scalable/apps ${ICON_DST}/symbolic/apps ${FLATPAK_ICON_DST}/64x64 ${FLATPAK_ICON_DST}/128x128
 install -D ${ICON_SRC}/scalable/apps/${FLATPAK_ID}.svg ${ICON_DST}/scalable/apps
@@ -15,5 +15,4 @@ install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.gschema.xml ${FLATPAK_
 mkdir -p ${FLATPAK_DEST}/share/appdata ${FLATPAK_DEST}/share/app-info/xmls
 gzip ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml --keep
 install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml.gz ${FLATPAK_DEST}/share/app-info/xmls
-install -D ${CARGO_TARGET_PATH}/Queries ${FLATPAK_DEST}/bin
-install -D ${CARGO_TARGET_PATH}/helper ${FLATPAK_DEST}/bin
+install -D ${CARGO_TARGET_PATH}/queries ${FLATPAK_DEST}/bin
