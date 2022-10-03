45e74d0a8f84fa9b421c7c12f706852535f1a3aa
Diego Lima <lima.ds@outlook.com>
2022-10-02 21:17:48 -0300

    Updated manifest

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@ PKG_CONFIG_PATH=/app/lib:/app/lib/pkgconfig
 ICON_SRC=/run/build/Queries/data/icons/hicolor
 ICON_DST=/app/share/icons/hicolor
 FLATPAK_ICON_DST=/app/share/app-info/icons/flatpak
-CARGO_TARGET_PATH=/run/build/Queries/target/release
+CARGO_TARGET_PATH=/run/build/queries/target/release
 
 mkdir -p ${ICON_DST}/scalable/apps ${ICON_DST}/symbolic/apps ${FLATPAK_ICON_DST}/64x64 ${FLATPAK_ICON_DST}/128x128
 install -D ${ICON_SRC}/scalable/apps/${FLATPAK_ID}.svg ${ICON_DST}/scalable/apps
