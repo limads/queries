d2ad4c84660d2b4c7e4eec793480b51e3f258e96
Diego Lima <lima.ds@outlook.com>
2022-10-07 15:13:25 -0300

    Changed build script

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
+# This is required for vendored git dependencies.
+cargo generate-lockfile --offline
+
+GIT_PREFIX=https://github.com/limads
+
 cargo build --release --offline --verbose \
     --config "source.vendored-sources.directory=\"queries-deps\"" \
     --config "source.crates-io.replace-with = \"vendored-sources\"" \
-    --config "source.\"https://github.com/limads/archiver.git\".git = \"https://github.com/limads/archiver.git\"" \
-    --config "source.\"https://github.com/limads/archiver.git\".replace-with = \"vendored-sources\"" \
-    --config "source.\"https://github.com/limads/papyri.git\".git = \"https://github.com/limads/papyri.git\"" \
-    --config "source.\"https://github.com/limads/papyri.git\".replace-with = \"vendored-sources\"" \
-    --config "source.\"https://github.com/limads/stateful.git\".git = \"https://github.com/limads/stateful.git\"" \
-    --config "source.\"https://github.com/limads/stateful.git\".replace-with = \"vendored-sources\"" \
+    --config "source.\"${GIT_PREFIX}/archiver.git\".git = \"${GIT_PREFIX}/archiver.git\"" \
+    --config "source.\"${GIT_PREFIX}/archiver.git\".replace-with = \"vendored-sources\"" \
+    --config "source.\"${GIT_PREFIX}/papyri.git\".git = \"${GIT_PREFIX}/papyri.git\"" \
+    --config "source.\"${GIT_PREFIX}/papyri.git\".replace-with = \"vendored-sources\"" \
+    --config "source.\"${GIT_PREFIX}/stateful.git\".git = \"${GIT_PREFIX}/stateful.git\"" \
+    --config "source.\"${GIT_PREFIX}/stateful.git\".replace-with = \"vendored-sources\"" \
 
 # [source.crates-io]
 # replace-with = "vendored-sources"
