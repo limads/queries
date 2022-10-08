63ec834282dede8dc514cc2f431a759caecd2e42
Diego Lima <lima.ds@outlook.com>
2022-10-08 08:05:21 -0300

    Changed manifest

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -5,10 +5,10 @@ cargo vendor vendored/deps
 cd vendored/deps
 
 unused=(
-    "winapi-x86_64-pc-windows-gnu" "winapi-i686-pc-windows-gnu" "winapi-0.2.8"  "winapi-build"  "winapi-util" "windows_i686_gnu"  
+    "winapi-x86_64-pc-windows-gnu" "winapi-i686-pc-windows-gnu" "winapi-build" "winapi-util" "windows_i686_gnu"  
     "windows_x86_64_gnu" "windows-sys" "windows_aarch64_msvc" "windows_x86_64_msvc" 
-    "windows_i686_msvc" "web-sys" "ws2_32-sys" "winapi" "wasi" "wasm-bindgen" "wasm-bindgen-backend" "wasm-bindgen-macro"
-    "wasm-bindgen-macro-support" "wasi-0.10.0+wasi-snapshot-preview1" "wasm-bindgen-shared" "vcpkg"
+    "windows_i686_msvc" "winapi" "wasi" "wasm-bindgen" "wasm-bindgen-backend" "wasm-bindgen-macro"
+    "wasm-bindgen-macro-support" "wasm-bindgen-shared" "vcpkg"
 )
 
 for i in "${unused[@]}"
