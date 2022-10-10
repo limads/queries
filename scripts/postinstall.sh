3994c0516d93db4f220d4da5f7fbd293b5c3d5dc
Diego Lima <lima.ds@outlook.com>
2022-10-10 00:26:37 -0300

    Updated postbuild

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,7 @@
 ICON_SRC=/run/build/Queries/data/icons/hicolor
 ICON_DST=/app/share/icons/hicolor
-FLATPAK_ICON_DST=/app/share/app-info/icons/flatpak
+
+# FLATPAK_ICON_DST=/app/share/app-info/icons/flatpak
 CARGO_TARGET_PATH=/run/build/Queries/target/release
 
 # mkdir -p ${ICON_DST}/scalable/apps ${ICON_DST}/symbolic/apps ${FLATPAK_ICON_DST}/64x64 ${FLATPAK_ICON_DST}/128x128
@@ -10,11 +11,13 @@ install -D ${ICON_SRC}/symbolic/apps/${FLATPAK_ID}-symbolic.svg ${ICON_DST}/symb
 # install -D ${ICON_SRC}/128x128/apps/${FLATPAK_ID}.png ${FLATPAK_ICON_DST}/128x128
 # mkdir -p ${FLATPAK_DEST}/share/applications ${FLATPAK_DEST}/share/glib-2.0/schemas
 install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.desktop ${FLATPAK_DEST}/share/applications
-install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.gschema.xml ${FLATPAK_DEST}/share/glib-2.0/schemas
+# install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.gschema.xml ${FLATPAK_DEST}/share/glib-2.0/schemas
+install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml ${FLATPAK_DEST}/share/metainfo
+
 # mkdir -p ${FLATPAK_DEST}/share/appdata ${FLATPAK_DEST}/share/app-info/xmls
 
 # gzip ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml --keep
 # install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml.gz ${FLATPAK_DEST}/share/app-info/xmls
 
-# mkdir -p ${FLATPAK_DEST}/bin
-install -D ${CARGO_TARGET_PATH}/queries ${FLATPAK_DEST}/bin/queries
+mkdir -p ${FLATPAK_DEST}/bin
+install -D ${CARGO_TARGET_PATH}/queries ${FLATPAK_DEST}/bin
