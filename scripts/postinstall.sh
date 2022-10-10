5960e681cc5c6b717c62ce34c2521e2d1967b5f5
Diego Lima <lima.ds@outlook.com>
2022-10-10 00:16:37 -0300

    Updated postbuild

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -12,7 +12,9 @@ install -D ${ICON_SRC}/symbolic/apps/${FLATPAK_ID}-symbolic.svg ${ICON_DST}/symb
 install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.desktop ${FLATPAK_DEST}/share/applications
 install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.gschema.xml ${FLATPAK_DEST}/share/glib-2.0/schemas
 # mkdir -p ${FLATPAK_DEST}/share/appdata ${FLATPAK_DEST}/share/app-info/xmls
-gzip ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml --keep
-install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml.gz ${FLATPAK_DEST}/share/app-info/xmls
+
+# gzip ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml --keep
+# install -D ${FLATPAK_BUILDER_BUILDDIR}/data/${FLATPAK_ID}.appdata.xml.gz ${FLATPAK_DEST}/share/app-info/xmls
+
 # mkdir -p ${FLATPAK_DEST}/bin
 install -D ${CARGO_TARGET_PATH}/queries ${FLATPAK_DEST}/bin/queries
